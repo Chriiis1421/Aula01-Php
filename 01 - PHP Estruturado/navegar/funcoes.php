89adf7675e999c543c22a393c8c50fe88c3af270
Chriiis1421 <106244596+Chriiis1421@users.noreply.github.com>
2023-08-15 12:35:17 -0300

    insertTable

--- 01 - PHP Estruturado/navegar/funcoes.php
+++ 01 - PHP Estruturado/navegar/funcoes.php
@@ -1,22 +1,19 @@
 <?php
 
-    
-
-
     function loadTable() {
-        
+
+        // Ler os arquivos e montar array
         $pessoas = array();
 		$fp = fopen('pessoas.txt', 'r');
 
-		if ($fp) {
+        if ($fp) {
 
-			while(!feof($fp)) {
+            while(!feof($fp)) {
 				$arr = array();
-				$cpf = fgets($fp);
+                $cpf = fgets($fp);
 				$dados = fgets($fp);
 				if(!empty($dados)) {
 					$arr = explode("#", $dados);
-                    // print_r($arr);
 					$pessoas[$cpf] = $arr;
 				}
 			}
@@ -24,7 +21,8 @@
 			fclose($fp);
 		}
 
-		foreach ($pessoas as $cpf => $dados) {
+        // Escrever o array
+        foreach ($pessoas as $cpf => $dados) {
 
 			if(!empty($dados)) {
 				echo "<tr>";
@@ -33,9 +31,46 @@
 					foreach ($dados as $valor) {
 						echo "<td>".$valor."</td>";
 					}
+
+					echo "<td>";
+						echo "<button type='submit' name='acao' value='alterar/'".$cpf.">";
+							echo "<span class='glyphicon glyphicon-pencil'></span>";
+						echo "</button>";
+						echo "&nbsp";
+						echo "<button type='submit' name='acao' value='remover/'".$cpf.">";
+							echo "<span class='glyphicon glyphicon-remove'></span>";
+						echo "</button>";
+					echo "</td>";
 				echo "</tr>";
 			}
 		}
+    
+
+		
     }
 
+    function insertTable() {
+        
+        $arr = array(
+            $_POST['cpf'] => array(
+                "nome" => $_POST['nome'],
+                "ender" => $_POST['ender'],
+                "tel" => $_POST['tel']
+            )
+
+        );
+    
+        $fp = fopen('pessoas.txt', 'a+');
+    
+        if ($fp) {
+            foreach($arr as $cpf => $dados) {
+                if(!empty($dados)) {
+                    fputs($fp, "$cpf\n");
+                    $linha = $dados['nome']."#".$dados['ender']."#".$dados['tel'];
+                    fputs($fp, "$linha\n");
+                }
+            }
+            fclose($fp);
+        }
+    }
 ?>
